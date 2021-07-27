f6ad042ef6095ec0daaaa87b7be17a10f866dd2a
Konvoir <baleskoff@gmail.com>
2021-07-27 14:40:17 +0300

    render markup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './css/styles.css';
 
+import API from './fetchCountries';
+import countryCard from './countryCard.hbs';
+import renderCountryList from './renderCountryList.hbs'
+import getRefs from './getRefs.js';
+import Notiflix from "notiflix";
+
+
+
 const DEBOUNCE_DELAY = 300;
